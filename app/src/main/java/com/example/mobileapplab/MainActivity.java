75075561b58bfcb53233995b8027ddbc363f105d
TomiArotiba <ob.arotiba@gmail.com>
2022-09-22 15:58:02 +0300

    Added Button and TextView elements with code that handles respective events.

--- app/src/main/java/com/example/mobileapplab/MainActivity.java
+++ app/src/main/java/com/example/mobileapplab/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.mobileapplab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textview;
+    Button showBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.name_txt);
+        showBtn = (Button) findViewById(R.id.show_name);
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview.setText("My name is Oluwatomiloba");
+            }
+        });
     }
 }
